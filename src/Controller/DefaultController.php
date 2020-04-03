9402bd59ac53b1bec95df736d5feaad7bbe805e3
Tr1nome <leosouly@gmail.com>
2020-04-03 15:40:38 +0200

    premier commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+/**
+ * @Route("/", name="accueilClass_", host="leo-souly-modula.fr")
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig');
+
+    }
+}

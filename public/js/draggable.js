88cf89b400a80dd48a3c6d6aaa58fab1a064d13d
Tr1nome <leosouly@gmail.com>
2020-04-04 19:33:58 +0200

    drag n drop save position

--- public/js/draggable.js
+++ public/js/draggable.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
-    $(".dragger").draggable({
-        containment:"body"
+
+    //grab
+    $(".dragger").mousedown(function(){
+        $(".dragger:hover").css( "cursor","grabbing"),cancelable = false;
+        
     });
+    //release
+    $(".dragger").mouseup(function(){
+        $(".dragger:hover").css( "cursor","grab");
+        
+    });
+
+    var positions = JSON.parse(localStorage.positions || "{}");
+    $(function () {
+    var d = $("[id=draggable]").attr("id", function (i) {
+        return "draggable_" + i
+    })
+    $.each(positions, function (id, pos) {
+        $("#" + id).css(pos)
+    })
+
+    d.draggable({
+        containment: "body",
+        scroll: false,
+        stop: function (event, ui) {
+            positions[this.id] = ui.position
+            localStorage.positions = JSON.stringify(positions)
+        }
+    });
+});
+    
 });
